874e3d5a20b71a88c6f1cb6750c1b1c189d43dca
m.faizan <muhammad.faizan@cloudclinik.com-at-230264426720>
2022-07-25 20:03:08 +0500

    live tracking sample code added. still needs more smoothness in the animation

---
+++ lib/tracking/tracking_screen.dart
@@ -0,0 +1,191 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:vector_math/vector_math.dart';
+
+class TrackingScreen extends StatefulWidget {
+
+  const TrackingScreen({Key? key}) : super(key: key);
+
+  @override
+  _TrackingScreenState createState() => _TrackingScreenState();
+}
+
+class _TrackingScreenState extends State<TrackingScreen> with TickerProviderStateMixin {
+  final List<Marker> _markers = <Marker>[];
+  Animation<double>? _animation;
+  late GoogleMapController _controller;
+
+  final _mapMarkerSC = StreamController<List<Marker>>();
+
+  StreamSink<List<Marker>> get _mapMarkerSink => _mapMarkerSC.sink;
+
+  Stream<List<Marker>> get mapMarkerStream => _mapMarkerSC.stream;
+
+  StreamSubscription<Position>? _locationStreamSubscription;
+  late Position currentPosition;
+  late BitmapDescriptor carImage;
+  @override
+  void initState() {
+    super.initState();
+    getBytesFromAsset('images/ic_car.png', 60).then((value) => carImage = BitmapDescriptor.fromBytes(value) );
+
+    Geolocator.requestPermission().then((value){
+
+    });
+  }
+  @override
+  void dispose() {
+    _locationStreamSubscription?.cancel();
+    super.dispose();
+  }
+  void registerLocationUpdates(){
+    Geolocator.getCurrentPosition().then((value){
+      currentPosition = value;
+
+      final positionStream = Geolocator.getPositionStream(locationSettings: const LocationSettings(
+          distanceFilter: 10,
+          accuracy: LocationAccuracy.bestForNavigation
+      ));
+
+      _locationStreamSubscription = positionStream.listen((Position position) async {
+        currentPosition  = position;
+        animateCar(
+            currentPosition.latitude, currentPosition.longitude,
+            position.latitude, position.longitude,
+            position.heading,
+            _mapMarkerSink, this, _controller
+        );
+      });
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    const currentLocationCamera = CameraPosition(
+      target: LatLng(37, 71),
+    );
+
+    final googleMap = StreamBuilder<List<Marker>>(
+        stream: mapMarkerStream,
+        builder: (context, snapshot) {
+          return GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: currentLocationCamera,
+            onMapCreated: (GoogleMapController controller) {
+              _controller = controller;
+              registerLocationUpdates();
+            },
+            markers: Set<Marker>.of(snapshot.data ?? []),
+            padding: const EdgeInsets.all(8),
+          );
+        });
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          googleMap,
+        ],
+      ),
+    );
+  }
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  animateCar(
+      double fromLat, //Starting latitude
+      double fromLong, //Starting longitude
+      double toLat, //Ending latitude
+      double toLong, //Ending longitude
+      double heading,
+      StreamSink<List<Marker>>
+      mapMarkerSink, //Stream build of map to update the UI
+      TickerProvider provider, //Ticker provider of the widget. This is used for animation
+      GoogleMapController controller, //Google map controller of our widget
+      ) async {
+    final double bearing = heading;
+
+    _markers.clear();
+
+    var carMarker = Marker(
+        markerId: const MarkerId("driverMarker"),
+        position: LatLng(fromLat, fromLong),
+        icon: carImage,
+        anchor: const Offset(0.5, 0.5),
+        flat: true,
+        rotation: bearing,
+        draggable: false);
+
+    //Adding initial marker to the start location.
+    _markers.add(carMarker);
+    mapMarkerSink.add(_markers);
+
+    final animationController = AnimationController(
+      duration: const Duration(seconds: 1), //Animation duration of marker
+      vsync: provider, //From the widget
+    );
+
+    Tween<double> tween = Tween(begin: 0, end: 1);
+
+    _animation = tween.animate(animationController)
+      ..addListener(() async {
+        //We are calculating new latitude and logitude for our marker
+        final v = _animation!.value;
+        double lng = v * toLong + (1 - v) * fromLong;
+        double lat = v * toLat + (1 - v) * fromLat;
+        LatLng newPos = LatLng(lat, lng);
+
+        //Removing old marker if present in the marker array
+        if (_markers.contains(carMarker)) _markers.remove(carMarker);
+
+        //New marker location
+        carMarker = Marker(
+            markerId: const MarkerId("driverMarker"),
+            position: newPos,
+            icon: carImage,
+            anchor: const Offset(0.5, 0.5),
+            flat: true,
+            rotation: bearing,
+            draggable: false);
+
+        //Adding new marker to our list and updating the google map UI.
+        _markers.add(carMarker);
+        mapMarkerSink.add(_markers);
+      });
+
+    //Starting the animation
+    animationController.forward();
+  }
+
+  double getBearing(LatLng begin, LatLng end) {
+    double lat = (begin.latitude - end.latitude).abs();
+    double lng = (begin.longitude - end.longitude).abs();
+
+    if (begin.latitude < end.latitude && begin.longitude < end.longitude) {
+      return degrees(atan(lng / lat));
+    } else if (begin.latitude >= end.latitude &&
+        begin.longitude < end.longitude) {
+      return (90 - degrees(atan(lng / lat))) + 90;
+    } else if (begin.latitude >= end.latitude &&
+        begin.longitude >= end.longitude) {
+      return degrees(atan(lng / lat)) + 180;
+    } else if (begin.latitude < end.latitude &&
+        begin.longitude >= end.longitude) {
+      return (90 - degrees(atan(lng / lat))) + 270;
+    }
+    return -1;
+  }
+}
